671582df5c6efc803e12f5f4014a416a111a1d41
OtakuNisha <136645632+OtakuNisha@users.noreply.github.com>
2025-06-02 15:37:45 -0700

    Create users.js

---
+++ server/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// User portfolio routes would go here
+// (Register, login, portfolio management, etc.)
+
+module.exports = router;
